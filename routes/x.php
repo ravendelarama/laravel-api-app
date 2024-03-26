0002521140c67e1e408140db03ce9872b1dc944b
Raven <official.ravendelarama@gmail.com>
2024-03-26 16:37:16 +0800

    Revised routes

--- routes/x.php
+++ routes/x.php
@@ -21,8 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::controller(PogiController::class)->group(function () {
-    Route::prefix('/users')->group(function () {
+Route::controller(PogiController::class)->prefix('/users')->group(function () {
         Route::get('/', 'index');
         Route::post('/', 'store');
         Route::patch('/edit', 'edit');
